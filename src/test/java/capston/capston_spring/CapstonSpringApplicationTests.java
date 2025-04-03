d5d21764bfe35d396cba145193aaa93a7a600955
yumin <ymdwang200@ewhain.net>
2025-04-03 17:02:21 +0900

    Initial commit to yumin

---
+++ src/test/java/capston/capston_spring/CapstonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package capston.capston_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstonSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
