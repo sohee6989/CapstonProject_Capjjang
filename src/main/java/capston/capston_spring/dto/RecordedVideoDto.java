aa20806d05024f87ea96345267e964a15fe40493
yumin <ymdwang200@ewhain.net>
2025-05-09 16:22:45 +0900

    프론트의 request에서 videoPath제거

--- src/main/java/capston/capston_spring/dto/RecordedVideoDto.java
+++ src/main/java/capston/capston_spring/dto/RecordedVideoDto.java
@@ -12,7 +12,6 @@ public class RecordedVideoDto {
     // id하나로 통일하고, ViewMode로 받기
     private Long SessionId;
     private VideoMode videoMode;
-    private String videoPath;
     private LocalDateTime recordedAt;
     private int duration;
 }
