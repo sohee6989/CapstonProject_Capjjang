590755c5efcee7a9fa1e78aefb41e8594dfc2181
Doyoon <nkdy08@gmail.com>
2025-05-11 01:11:42 +0900

    GPT 연동 관련 코드 추가 및 수정

--- src/main/java/capston/capston_spring/dto/ChatCompletionRequest.java
+++ src/main/java/capston/capston_spring/dto/ChatCompletionRequest.java
@@ -4,12 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
-import java.util.Map;
 
 @Getter
 @Setter
 public class ChatCompletionRequest {
     private String model;
-    private List<Map<String, String>> messages;
-    private double temperature;
+    private List<ChatMessage> messages;
+    private Double temperature = 1.0;  // optional
 }
